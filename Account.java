d4b15eeda4e017113e03d6a9991fe1c2e5fbd1e9
OlawamideMoyinoluwa <134518428+MOYIN-21@users.noreply.github.com>
2023-05-29 16:41:44 +0100

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account {
+private String name;
+	public Account(String name) {
+		this.name = name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
