d6a48a03ac00a7981c3c6f524639012ca8a3c620
OlawamideMoyinoluwa <134518428+MOYIN-21@users.noreply.github.com>
2023-05-31 12:54:54 +0100

    Add files via upload

---
+++ AccountTest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AccountTest {
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	
+	Account myAccount = new Account();
+	
+	System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+	
+	System.out.println("please enter the name:");
+	String theName = input.nextLine();
+	myAccount.setName(theName);
+	System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+	
+	}	
+}
